44af24145c336b008b667b5cd8e1c7c1a2a97dd0
imaginatio <sales.imaginatio@gmail.com>
2014-02-17 10:26:05 -0200

    new folders

--- public/index.php
+++ public/index.php
@@ -1,12 +1,18 @@
 <?php
   $requestURI = explode('/', $_SERVER['REQUEST_URI']);
-  
+  $file_path = $_SERVER['DOCUMENT_ROOT']. "/laboratorios/app/controllers/". $requestURI[2] ."_controller.php";
+  echo($_SERVER['DOCUMENT_ROOT']);
+  echo("<br/>");
+  echo($_SERVER['SCRIPT_FILENAME']);
+  echo("<br/>");
+  echo($file_path);
+  echo("<br/>");
   try{
-    if (!file_exists("./app/controllers/". $requestURI[2] ."_controller.php")){
+    if (!file_exists($file_path)){
       throw new Exception("Routing Error.");
     }
     else{
-      require_once "./app/controllers/". $requestURI[2] ."_controller.php";
+      require_once  $file_path;
       if(!function_exists ($requestURI[3])){
         throw new Exception("Routing Error.");
       }else{
