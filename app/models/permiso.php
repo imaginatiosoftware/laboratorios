00a36b24afc23aad0f641354bf1ab46cfde17bac
Álvaro C <correapereira.alvarojavier@gmail.com>
2014-03-13 08:07:11 -0300

    Commit de ayer que nunca hice

---
+++ app/models/permiso.php
@@ -0,0 +1,4 @@
+<?php 
+  class Permiso extends Model {
+  }
+?>
--- config/db/bd.sql
+++ config/db/bd.sql
@@ -1,9 +1,33 @@
 DROP laboratorios_development IF EXISTS;
 CREATE DATABASE laboratorios_development;
 
+USE laboratorios_development;
+
+CREATE TABLE permisos (
+  id          INT              NOT NULL AUTO_INCREMENT,
+  descripcion VARCHAR  ( 255 ) NOT NULL,
+  habilitado  TINYINT  ( 1   ) NOT NULL,
+  PRIMARY KEY ( id )
+);
+
+CREATE TABLE roles (
+  id          INT              NOT NULL AUTO_INCREMENT,
+  descripcion VARCHAR  ( 255 ) NOT NULL,
+  habilitado  TINYINT  ( 1   ) NOT NULL,
+  PRIMARY KEY ( id )
+);
+
+CREATE TABLE roles_permisos (
+  rol_id      INT NOT NULL,
+  permiso_id  INT NOT NULL,
+  FOREIGN KEY ( rol_id     ) REFERENCES roles    ( id ),
+  FOREIGN KEY ( permiso_id ) REFERENCES permisos ( id ),
+  PRIMARY KEY ( rol_id, permiso_id )
+);
+
 CREATE TABLE usuarios (
-  id                   LONG            NOT NULL AUTO_INCREMENT,
-  nombre               VARCAHR ( 255 ) NOT NULL,
+  id                   INT             NOT NULL AUTO_INCREMENT,
+  nombre               VARCHAR ( 255 ) NOT NULL,
   apellido             VARCHAR ( 255 ) NOT NULL,
   segundo_apellido     VARCHAR ( 255 ) NOT NULL,
   cedula               VARCHAR ( 255 ) NOT NULL,
@@ -11,8 +35,8 @@ CREATE TABLE usuarios (
   direccion            VARCHAR ( 255 ) NOT NULL,
   telefono             VARCHAR ( 255 ) NOT NULL,
   email                VARCHAR ( 255 ) NOT NULL,
-  rol_id               LONG            NOT NULL,
-  habilitado           TINY INT        NOT NULL,
-  FOREIGN KEY          rol_id          REFERENCES roles( id ),
+  rol_id               INT             NOT NULL,
+  habilitado           TINYINT ( 1   ) NOT NULL,
+  FOREIGN KEY          ( rol_id )      REFERENCES roles( id ),
   PRIMARY KEY ( id )
 );
